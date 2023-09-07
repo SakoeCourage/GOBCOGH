434129c464f073d1741f955121bd9d4fcaa257c4
sakoe courage <akorlicourage@gmail.com>
2023-09-07 19:25:24 +0000

    check eslint characters

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import Header from './header'
-import Footer from './footer'
+import Header from './Header'
+import Footer from './Footer'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
