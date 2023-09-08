4986bdc8ab5c81d08532ab10a6940e64c6e6c5a4
sakoe courage <akorlicourage@gmail.com>
2023-09-08 11:28:03 +0000

    dynamic import for apex chart

--- app/projects/Projectpercentage.tsx
+++ app/projects/Projectpercentage.tsx
@@ -1,33 +1,38 @@
 "use client"
-import React, { useState, useCallback } from 'react'
-import ReactApexChart from 'react-apexcharts'
-import {Range,statsGroup,RangeoptionsProps} from './projects.td'
-export const colors_sets:string[] = ['#0284c7', '#fb7185', '#fde047', '#92400e', '#15803d', '#312e81', '#fda4af', '#1e293b', '#84cc16', '#312e81']
+import React, { useState } from 'react'
+import dynamic from 'next/dynamic'
 
+const ReactApexChart = dynamic(() => import('react-apexcharts'), { ssr: false });
 
-let state = {
-    series: [0, 85.7, 14.3],
-    options: {
-        legend: {
-            fontSize: '20px'
-        },
-       
-        labels: ["Commerical Projects", "Industrial Projects", "Civil Projects"],
-        colors: [...colors_sets],
-        responsive: [{
-            breakpoint: 480,
-            options: {
-                legend: {
-                    position: "bottom"
-                },
+type Range = "2000-2005" | "2006-2010" | "2011-2014";
 
-            }
-        }]
-    }
+type stats = { range: Range, series: Array<number> }
+
+const statsGroup: stats[] = [
+    {
+        range: "2000-2005",
+        series: [0, 85.7, 14.3]
+    },
+    {
+        range: "2006-2010",
+        series: [5, 37.5, 57.5]
+    },
+    {
+        range: "2011-2014",
+        series: [31.8, 32, 36.2]
+    },
+]
+interface RangeoptionsProps {
+    active: boolean;
+    range: Range;
+    setNewRange: (arg0: Range) => void;
 }
 
+const colors_sets: string[] = ['#0284c7', '#fb7185', '#fde047', '#92400e', '#15803d', '#312e81', '#fda4af', '#1e293b', '#84cc16', '#312e81']
 
-function Rangeoptions({ active = false, range ="2000-2005", setNewRange }: RangeoptionsProps) {
+
+
+function Rangeoptions({ active = false, range = "2000-2005", setNewRange }: RangeoptionsProps) {
     return <button onClick={() => setNewRange(range)} className={`flex items-center gap-3 text-lg py-3 transition-transform duration-200 transform ${active == true ? '!text-primary-orange underline  scale-105 font-bold text-2xl' : 'text-gray-400 hover:text-primary-orange/60 hover:underline'}`}>
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="m9.55 18l-5.7-5.7l1.425-1.425L9.55 15.15l9.175-9.175L20.15 7.4L9.55 18Z" /></svg>
         <nav className=''>
@@ -38,6 +43,27 @@ function Rangeoptions({ active = false, range ="2000-2005", setNewRange }: Range
 
 
 function Projectpercentage() {
+    let state = {
+        series: [0, 85.7, 14.3],
+        options: {
+            legend: {
+                fontSize: '20px'
+            },
+
+            labels: ["Commerical Projects", "Industrial Projects", "Civil Projects"],
+            colors: [...colors_sets],
+            responsive: [{
+                breakpoint: 480,
+                options: {
+                    legend: {
+                        position: "bottom"
+                    },
+
+                }
+            }]
+        }
+    }
+
     const [currentState, setCurrentState] = useState(state);
     const [currentRange, setCurrentRange] = useState<Range>("2000-2005");
 
